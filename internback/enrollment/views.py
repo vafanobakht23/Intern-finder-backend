6782f0be5afcac6b8fd83b4b51ab772cc6ae8643
vafanobakht23 <vafa.n@greenhabitat.at>
2023-09-25 19:17:55 +0330

    feat: create enrollment view

--- internback/enrollment/views.py
+++ internback/enrollment/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+# views.py
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Enrollment
+from .serializers import EnrollmentSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+
+    def perform_create(self, serializer):
+        # Customize the create behavior if necessary
+        post_id = request.data.get("post")
+        user_id = request.data.get("user_id")
+        # Check if the post with the given post_id exists
+        try:
+            post = Post.objects.get(pk=post_id)
+        except Post.DoesNotExist:
+            return Response(
+                {"detail": "Post with this post_id does not exist."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return Response(
+                {"detail": "User with this user_id does not exist."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def perform_update(self, serializer):
+        # Customize the update behavior if necessary
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        # Customize the delete behavior if necessary
+        instance.delete()
